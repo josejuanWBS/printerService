00ff1aec203b14899b58104b7c6a08b1d9106597
JoseJuan98 <josejuan-98@hotmail.com>
2021-01-13 23:12:19 +0100

    TravisCI v2.0

--- test/test_formater.py
+++ test/test_formater.py
@@ -26,24 +26,3 @@ class Test_Formater:
     def test_get_formatted(self, name, msg):
         r = get_formatted(msg, name, "plain_uppercase")
         assert (r == 'JOSE MESSAGE'), f' Failing to convert to upper case the variable name with the value {name} or the variable msg with the value {msg}'
-
-    # To be executed by a command line:
-    # Linux:                            # Windows
-    # cd directory_of_project           # dir directory_of_project
-    # python3 -m pytest test/
-    # It should display something like:
-    #
-# (base) user@pc:~/Projects/printerService$ python3 -m pytest -v test/
-# ====================================================================== test session starts ======================================================
-# platform linux -- Python 3.8.3, pytest-6.2.1, py-1.9.0, pluggy-0.13.1
-# rootdir: /home/Projects/printerService
-# collected 7 items
-#
-# test/test_formater.py::Test_Formater::test_plain_uppercase PASSED                                                                          [ 14%]
-# test/test_formater.py::Test_Formater::test_plain_text PASSED                                                                               [ 28%]
-# test/test_formater.py::Test_Formater::test_get_formatted PASSED                                                                            [ 42%]
-# test/test_views.py::Test_Flask_Views::test_output PASSED                                                                                   [ 57%]
-# test/test_views.py::Test_Flask_Views::test_outputs PASSED                                                                                  [ 71%]
-# test/test_views.py::Test_Flask_Views::test_form PASSED                                                                                     [ 85%]
-# test/test_views.py::Test_Flask_Views::test_mult PASSED                                                                                     [100%]
-#======================================================================= 7 passed in 0.09s ========================================================
