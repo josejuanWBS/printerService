f57359ddf8c9f48dfa9f761eb97d53aec79737ef
JoseJuan98 <josejuan-98@hotmail.com>
2021-01-31 14:50:46 +0100

    deleted pycache

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from service.views import app
 
-#app must be initialized in the same directory than the views
+# the app variable must be initialized in the same directory than the views
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
